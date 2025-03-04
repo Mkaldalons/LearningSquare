175a2e8083b4996da13239c162d40027a8dd4df5
Mkaldalons <mrk9@hi.is>
2025-03-04 21:52:45 +0000

    More graceful error handling for login and signup

--- app/src/main/java/hbv601g/learningsquare/ui/LoginFragment.kt
+++ app/src/main/java/hbv601g/learningsquare/ui/LoginFragment.kt
@@ -6,10 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import hbv601g.learningsquare.R
-import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
-import androidx.collection.emptyLongSet
 import androidx.lifecycle.lifecycleScope
 import hbv601g.learningsquare.services.HttpsService
 import hbv601g.learningsquare.services.UserService
@@ -29,12 +26,12 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         val errorTextView = view.findViewById<TextView>(R.id.errorTextView)
         val signupButton = view.findViewById<Button>(R.id.signupButton)
 
-        //Log.d("LoginFragment", "onCreateViewExecuted")
+        var errorTextString = ""
 
         loginButton.setOnClickListener {
             val username = inputUsername?.text.toString()
             val password = inputPassword?.text.toString()
-            //Log.d("LoginFragment", "ButtonClicked")
+
             if (username.isNotBlank() && password.isNotBlank()) {
                 lifecycleScope.launch {
                     val httpsService = HttpsService()
@@ -43,7 +40,6 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                     val user = userService.loginUser(username, password)
                     if (user != null)
                     {
-                        //Log.d("LoginFragment", "Validated tag: ${user.isInstructor}")
                         if (user.isInstructor) {
                             parentFragmentManager.beginTransaction()
                                 .replace(R.id.fragment_container_view, InstructorDashboardFragment())
@@ -52,23 +48,38 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                         }
                         else
                         {
+                            errorTextString = "Dashboard is currently only available to instructors"
                             errorTextView.visibility = View.VISIBLE
-                            errorTextView.text = "Dashboard is currently only available to instructors"
+                            errorTextView.text = errorTextString
+                            errorTextView.postDelayed({
+                                errorTextView.visibility = View.GONE
+                            }, 5_000)
                         }
                     }
                     else {
+                        errorTextString = "Wrong username or password"
                         errorTextView.visibility = View.VISIBLE
-                        errorTextView.text = "Wrong username or password"
+                        errorTextView.text = errorTextString
+                        errorTextView.text = errorTextString
+                        errorTextView.postDelayed({
+                            errorTextView.visibility = View.GONE
+                        }, 5_000)
+                        inputUsername.text.clear()
+                        inputPassword.text.clear()
                     }
                 }
             }
             else {
+                errorTextString = "Please enter username and password"
                 errorTextView.visibility = View.VISIBLE
-                errorTextView.text = "Please enter username and password"
+                errorTextView.text = errorTextString
+                errorTextView.text = errorTextString
+                errorTextView.postDelayed({
+                    errorTextView.visibility = View.GONE
+                }, 5_000)
             }
         }
         signupButton.setOnClickListener {
-            //Log.d("LoginFragment", "Signup button clicked")
             lifecycleScope.launch {
                 parentFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container_view, SignupFragment())
