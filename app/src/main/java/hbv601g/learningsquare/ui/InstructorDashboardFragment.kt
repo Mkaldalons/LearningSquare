03128f06f3b1d246efb371ea2db9de48b1331189
Arnaeiriks <123424385+Arnaeiriks@users.noreply.github.com>
2025-03-07 21:31:33 +0000

    creating assignments

--- app/src/main/java/hbv601g/learningsquare/ui/InstructorDashboardFragment.kt
+++ app/src/main/java/hbv601g/learningsquare/ui/InstructorDashboardFragment.kt
@@ -1,12 +1,22 @@
 package hbv601g.learningsquare.ui
 
+
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import hbv601g.learningsquare.R
+import android.widget.Button
 
 class InstructorDashboardFragment : Fragment(R.layout.fragment_instructor_layout) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val buttonCourses = view.findViewById<Button>(R.id.buttonCourses)
+        buttonCourses.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, CoursesFragment())
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
